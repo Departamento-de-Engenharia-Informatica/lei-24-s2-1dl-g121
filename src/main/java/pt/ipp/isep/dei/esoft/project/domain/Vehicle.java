5695022490150e9ba533710ede4081c1e41fadcf
1230561@isep.ipp.pt <1230561@isep.ipp.pt>
2024-05-23 09:30:25 +0100

    Vehicle class created

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Vehicle.java
@@ -0,0 +1,79 @@
+package pt.ipp.isep.dei.esoft.project.domain;
+
+public class Vehicle {
+    private String brand;
+    private String model;
+    private String type;
+    private double tareWeight;
+    private double grossWeight;
+    private double currentKm;
+    private String registerDate;
+    private String acquisitionDate;
+    private double maintenanceFrequencyKm;
+
+    public Vehicle(String brand, String model, String type, double tareWeight, double grossWeight, String registerDate, String acquisitionDate, double maintenanceFrequencyKm) {
+        this.brand = brand;
+        this.model = model;
+        this.type = type;
+        this.tareWeight = tareWeight;
+        this.grossWeight = grossWeight;
+        this.currentKm = currentKm;
+        this.registerDate = registerDate;
+        this.acquisitionDate = acquisitionDate;
+        this.maintenanceFrequencyKm = maintenanceFrequencyKm;
+    }
+    public double getCurrentKm() {
+        return currentKm;
+    }
+    public double getGrossWeight() {
+        return grossWeight;
+    }
+    public double getTareWeight() {
+        return tareWeight;
+    }
+    public double getMaintenanceFrequencyKm() {
+        return maintenanceFrequencyKm;
+    }
+    public String getRegisterDate() {
+        return registerDate;
+    }
+    public String getAcquisitionDate() {
+        return acquisitionDate;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setAcquisitionDate(String acquisitionDate) {
+        this.acquisitionDate = acquisitionDate;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public void setCurrentKm(double currentKm) {
+        this.currentKm = currentKm;
+    }
+    public void setGrossWeight(double grossWeight) {
+        this.grossWeight = grossWeight;
+    }
+    public void setMaintenanceFrequencyKm(double maintenanceFrequencyKm) {
+        this.maintenanceFrequencyKm = maintenanceFrequencyKm;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setRegisterDate(String registerDate) {
+        this.registerDate = registerDate;
+    }
+    public void setTareWeight(double tareWeight) {
+        this.tareWeight = tareWeight;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+}
